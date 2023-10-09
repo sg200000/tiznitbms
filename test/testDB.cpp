1ba2004555b028e981a3e4385869488c433971a3
sg200000 <saidguouihaj@gmail.com>
2023-10-09 14:11:59 +0200

    final changes

--- test/testDB.cpp
+++ test/testDB.cpp
@@ -1,19 +1,19 @@
+#include "../inc/sqlite3db.hpp"
 #include <string>
 #include <unordered_map>
-#include "../inc/sqlite3db.hpp"
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN 
 #include "doctest.h"
 
 TEST_CASE("Test URID")
 {
     Sqlite3DB db("../../bank.db");
-    bool deleted = db.deleteData("accounts","id","6336");
+    bool deleted = db.deleteData("accounts",{{"id","6336"}});
     CHECK(deleted == true);
-    bool updated = db.updateData("accounts", "id","150","id","104");
+    bool updated = db.updateData("accounts", {{"id","150"}},{{"id","104"}});
     CHECK(updated == true);
 
     std::unordered_map<std::string,std::string> data = {
-        {"id", "3455606500"},
+        {"id", "3455606546500"},
         {"balance", "0"},
         {"min", "0"}
     };
@@ -22,7 +22,7 @@ TEST_CASE("Test URID")
 
     std::vector<std::vector<std::string>> outData;
     std::vector<std::string> columns = {"id"};
-    bool requested = db.requestData("accounts", columns, {{"balance", "0.0"}, {"min", "0.0"}}, outData);
+    bool requested = db.requestData("accounts", columns, {{"balance", "0.0"}, {"min", "0.0"}}, &outData);
     CHECK(requested);
     for (auto row : outData){
         for (auto val : row){
